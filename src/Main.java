75bec9c343311582ee6c70978692966ba21474ea
musawerbidar <bidarmusawer2015@gmail.com>
2023-05-09 14:19:43 -0700

    made start screen, added new window for game

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame frame = new JFrame("AIM LABS");
+        frame.setLayout(new BorderLayout());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel welcome = new JLabel("  Welcome to the Aim Lab. Click any mode to begin.");
+        welcome.setFont(new Font("Arial", Font.PLAIN, 32));
+        frame.add(welcome, BorderLayout.NORTH);
+        JPanel buttons = new JPanel();
+        JButton normal = new JButton("Normal");
+        buttons.add(normal);
+        frame.add(buttons);
+        normal.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                NormalMode game = new NormalMode();
+            }
+        });
+
+        frame.setSize(760, 500);
+        frame.setVisible(true);
     }
 }
+
---
+++ src/NormalMode.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class NormalMode {
+    public NormalMode(){
+        JFrame frame = new JFrame("AIM LABS");
+        frame.setLayout(new BorderLayout());
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+    }
+}

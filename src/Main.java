df078a7b677f0132be8663f13f674929e0ee2424
musawerbidar <bidarmusawer2015@gmail.com>
2023-05-11 09:03:07 -0700

    normalmode screen ready to start spawning objects

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.IOException;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -13,12 +17,21 @@ public class Main {
         frame.add(welcome, BorderLayout.NORTH);
         JPanel buttons = new JPanel();
         JButton normal = new JButton("Normal");
-        buttons.add(normal);
+        buttons.add(normal, BorderLayout.CENTER);
         frame.add(buttons);
         normal.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
                 NormalMode game = new NormalMode();
             }
+        });
+        frame.addMouseListener(new MouseListener() {
+            public void mouseClicked(MouseEvent e) {
+
+            }
+            public void mouseEntered(MouseEvent e) {}
+            public void mouseExited(MouseEvent e) {}
+            public void mousePressed(MouseEvent e) {}
+            public void mouseReleased(MouseEvent e) {}
         });
 
         frame.setSize(760, 500);
--- src/NormalMode.java
+++ src/NormalMode.java
@@ -1,11 +1,58 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+public class NormalMode implements MouseListener {
+    JFrame frame;
+    JLabel startMessage;
 
-public class NormalMode {
+    static boolean click;
     public NormalMode(){
-        JFrame frame = new JFrame("AIM LABS");
-        frame.setLayout(new BorderLayout());
-        frame.setSize(500, 500);
+        click = false;
+        frame = new JFrame("AIM LABS");
+        // frame.setLayout(new BorderLayout());
+        startMessage = new JLabel("\t \t \t Blobs will randomly spawn on the screen. " +
+                "\n Click them as fast as possible. " +
+                "\n Click anywhere to begin.");
+        startMessage.setFont(new Font("Arial", Font.PLAIN, 20));
+        frame.add(startMessage, BorderLayout.CENTER);
+
+        frame.setSize(1920, 1080);
         frame.setVisible(true);
+
+        frame.addMouseListener(this);
+    }
+
+    public static void setClicked(){
+        click = true;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        frame.remove(startMessage);
+        frame.repaint();
+        System.out.println("hi");
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
 }
